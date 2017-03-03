5df2a55c3aaca2f5df082365b3e4fa46600caa59
Brian Frick <helmethair@zoho.com>
2017-03-02 22:57:45 -0600

    blah blah

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -17,18 +17,24 @@ function doSearch() {
         }
         
         
-function fillDataByTitle() {
+function fillDataByTitle(finput) {
         	//var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=e34f926e66fa7ffcaaea86c905cf10de%26query="
         	//queryURL += "braveheart";
         	
-        	$.getJSON ("https://api.themoviedb.org/3/search/movie?api_key=e34f926e66fa7ffcaaea86c905cf10de",
+        	$.getJSON ("https://api.themoviedb.org/3/search/movie?api_key=e34f926e66fa7ffcaaea86c905cf10de&query=" + finput,
         	
-        				{query: $('#searchBox2').val()},
+        				//{query: $(finput)},
         				
         				function(data) {
         				    $(data.results).each(function(id, title){
 						    	console.log("hey" + this.title);
 						    	console.log(this.id);		
         					
-        				}); 
-    	}
+        				}); //close function
+	                    });//close getjson close fillDataByTitle
+        }
+        
+function test(stringthis){
+    console.log(stringthis + " is a string");
+    
+}
